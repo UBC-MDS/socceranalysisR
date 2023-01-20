240ec2c374cf57f4a7cf59847f3974f385f3bdfa
flora19 <flora19@student.ubc.ca>
2023-01-20 09:51:28 -0800

    Adding function docstring

---
+++ R/playerranking.R
@@ -0,0 +1,15 @@
+#' Ranking soccer players
+#'
+#' Given a soccer dataset, as well as column names, the function will rank the
+#' players by their names based on the column selected by the user.
+
+#' @param df Dataframe containing players information
+#' @param col_name A column name in the dataframe to be used to rank the players
+
+#' @return Returns a dataframe of names representing the ranking of the players based on the specified columns
+
+#' @examples
+#' rankingplayers(df,"Goals_total","Assists_Total")
+rankingplayers <- function(df, col_name, ...){
+
+}
