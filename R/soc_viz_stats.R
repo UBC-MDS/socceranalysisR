65afb4896efe8cd4a284827c35d1e5c2789e387b
Gaoxiang Wang <louiewang820@yahoo.com>
2023-01-19 18:49:40 -0800

    Add viz functions and update readme accordingly

---
+++ R/soc_viz_stats.R
@@ -0,0 +1,14 @@
+#' Create a scatter plot of or two histograms based on chosen numerical features.
+#' @param xcol Column vector in the dataframe based on which outliers are to be identified.
+#' @param ycol Column vector in the dataframe based on which outliers are to be identified.
+#' @param if_scatter determine if the user want to have scatter plots or histograms.
+#' @param df  Dataframe in  which vizs are to be generated.
+#'
+#' @return ggplot2 chart
+#' @export
+#'
+#' @examples
+#' soc_viz_stats('age', 'Wages_Euros', TRUE , data)
+soc_viz_stats <- function(xcol, ycol,if_scatter, df=data) {
+
+}
